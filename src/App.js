59b34893d24344e1296a455e59c20691fb1c728a
ankushkatiyar <ankushkatiyar@cedcommerce.com>
2023-09-18 14:07:01 +0530

    checking git commit with particular user name

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const handler = (event) => {
+    console.log(event.target.files[0]);
+    if (event.target.files && event.target.files[0]) {
+      let img = event.target.files[0];
+      console.log(URL.createObjectURL(img));
+    }
+  };
+  console.log("userName");
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="file" onChange={handler} />
       </header>
     </div>
   );
